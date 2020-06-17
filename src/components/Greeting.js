1642d0b10c559ca7261f57304ab35a52ccf7ac76
Roger Robertson Jr <rjrjr@pm.me>
2020-06-17 16:33:32 -0700

    updated greeting

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -5,8 +5,9 @@ const Greeting = (conversations) => {
     return(
         <div className="login_container">   
             <div className="no_conversations">
-                Welcome to Emissary, click on the menu to get started.
+                <p>Welcome to Emissary, click on the menu to get started.</p>
                 {/* Add all active users... */}
+                <p>Please refresh periodically to check for newly created or deleted conversations. This bug will be fixed in a future release.</p>
             </div>
         </div>
     )
